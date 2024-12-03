2c34137e0460056556d224fbc51d54d5d351e96f
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2024-12-03 18:39:58 +0100

    improved the calendar's grid

---
+++ calendario.js
@@ -0,0 +1,10 @@
+// funzione per prendere elementi da classe
+function getElementsByClass(classe) {
+    return document.getElementsByClassName(classe);
+}
+
+let cell = getElementsByClass('casella');
+
+for(let i = 1; i < cell.length + 1; i++){
+    cell[i - 1].innerText = i;
+}
