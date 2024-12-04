1e1c60059e645174d152badf0df46467ae154a4c
Riglietti-Nicolo <riglietti.nicolo@istitutoagnelli.it>
2024-12-04 13:38:00 +0100

    modified

--- calendario.js
+++ calendario.js
@@ -13,3 +13,10 @@ let cell = getElementsByClass('casella');
 for(let i = 1; i < cell.length + 1; i++){
     cell[i - 1].innerText = i;
 }
+
+
+function id(){
+    let anno = data_ora.getFullYear()
+    let primo_dicembre = new Date(`${anno}-08-26`);
+    console.log(primo_dicembre.getMilliseconds())
+}
