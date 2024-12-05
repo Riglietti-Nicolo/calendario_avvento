fd348411ab9a30751a0bc2f09649e04e05697126
Riglietti-Nicolo <riglietti.nicolo@istitutoagnelli.it>
2024-12-05 19:00:55 +0100

    add function id & checkDay

--- calendario.js
+++ calendario.js
@@ -15,7 +15,43 @@ for(let i = 1; i < cell.length + 1; i++){
 }
 
 function id(){
-    let anno = data_ora.getFullYear()
-    let primo_dicembre = new Date(`${anno}-08-26`);
-    console.log(primo_dicembre.getMilliseconds())
+    let anno = new Date().getFullYear();
+
+    let caselle = document.getElementsByClassName("casella")
+
+    Array.from(caselle).forEach((casella, index) => {
+        let data = new Date(anno, 11, index + 1);
+        let giorno = data.getDate();
+        let mese = data.getMonth();
+        let annoCorrente = data.getFullYear();
+        
+        casella.id = `${giorno}-${mese}-${annoCorrente}`;
+
+        casella.addEventListener("click", function(){            
+            checkDay(casella);
+        });
+    });
 }
+
+
+function checkDay(casella){
+
+    let oggi = new Date();
+    let giorno = oggi.getDate();
+    let mese = oggi.getMonth();
+    let anno = oggi.getFullYear();
+
+    let dataCorrente = `${giorno}-${mese}-${anno}`;
+
+    
+    if(dataCorrente === casella.id){
+        alert("ok");
+    }else{
+        alert("no");
+    }
+}
+
+id();
+
+
+
